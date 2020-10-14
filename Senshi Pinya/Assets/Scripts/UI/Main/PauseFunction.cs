504117c21a1e091c16d9a9d281cd2db074f766f7
Raphael Antonio <raphaelsebastian.antonio@benilde.edu.ph>
2020-10-14 16:01:51 +0800

    Pause Menu and Main Menu
    Created functinal Pause Menu and Main Menu buttons

---
+++ Senshi Pinya/Assets/Scripts/UI/Main/PauseFunction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class PauseFunction : MonoBehaviour
+{
+    public GameObject pauseBtn;
+    public GameObject PauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseBtn.SetActive(true);
+        PauseMenu.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0.0f;
+        pauseBtn.SetActive(false);
+        PauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+        pauseBtn.SetActive(true);
+        PauseMenu.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}

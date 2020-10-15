ae6a5b42d7d25efa643265b78a044bbc02bc189d
DESKTOP-ILOSGM9\Joben Maratas <joben_maratas@yahoo.com.ph>
2020-10-15 11:43:20 +0800

    Added Player Movement

---
+++ Senshi Pinya/Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Vector3 targetPoint;
+    private bool isMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            targetPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPoint.z = transform.position.z;
+            print("Left click!");
+        }
+
+        Movement();
+    }
+
+    void Movement()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPoint, speed * Time.deltaTime);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPoint);
+    }
+}

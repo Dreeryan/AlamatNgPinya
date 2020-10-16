5dce883e6d86ab964fb8dfeade573a0ad1ea8b66
Raphael Antonio <raphaelsebastian.antonio@benilde.edu.ph>
2020-10-16 16:31:15 +0800

    Implemented disappearing objective list in UI Test Scene

---
+++ Senshi Pinya/Assets/Scripts/UI/Main/AskObjective.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AskObjective : MonoBehaviour
+{
+    [SerializeField] GameObject objectives;
+    [SerializeField] float currentSeconds;
+    [SerializeField] float resetTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSeconds = 0;
+        objectives.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Time remaining until objective disappears: " + currentSeconds);
+        currentSeconds -= 1 * Time.deltaTime;
+
+        if (currentSeconds <= 0)
+        {
+            currentSeconds = 0;
+            objectives.SetActive(false);
+        }
+    }
+
+    public void StartTime()
+    {
+        currentSeconds = resetTime;
+        objectives.SetActive(true);
+    }
+}

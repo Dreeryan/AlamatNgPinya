4c48f8da51243679875b657e2a073542916fcfdb
Raphael Antonio <raphaelsebastian.antonio@benilde.edu.ph>
2020-10-16 15:14:33 +0800

    Implemented Click Interaction with UnityEvent in UI Test script

---
+++ Senshi Pinya/Assets/Scripts/Main/Events.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Events : MonoBehaviour
+{
+    [SerializeField] GameObject object1;
+    public UnityEvent OnObjectClick;
+
+    private void OnMouseOver()
+    {
+        Debug.Log("Cursor above " + gameObject.name);
+        if (Input.GetMouseButtonDown(0)) OnObjectClick.Invoke();
+    }
+
+}

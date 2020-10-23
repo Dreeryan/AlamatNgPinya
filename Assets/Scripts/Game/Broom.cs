795f241be491fd56015d4a80f0f1aeff9f1d443e
Raphael Antonio <raphaelsebastian.antonio@benilde.edu.ph>
2020-10-23 11:28:30 +0800

    Implemented broom sweeping into Floor Sweeping Scene

---
+++ Assets/Scripts/Game/Broom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Broom : MonoBehaviour
+{
+    private Vector2 mousePos;
+    private Vector2 currentPosition;
+
+    void Start()
+    {
+        currentPosition = transform.position;
+    }
+
+    void OnMouseDrag()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
+    }
+}
---
+++ Assets/Scripts/Game/SweepableObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SweepableObject : MonoBehaviour
+{
+    [SerializeField] private GameObject broom;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Goal")
+        {
+            Debug.Log("Sweeped to the right spot");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.tag == "Broom")
+        //{
+        //    var magnitude = 10000;
+
+        //    var force = transform.position - GameObject.FindWithTag("Broom").transform.position;
+
+        //    force.Normalize();
+        //    GetComponent<Rigidbody2D>().AddForce(-force * magnitude);
+        //}
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        var magnitude = 500; // Strength of the push
+
+        var force = transform.position - broom.transform.position;
+
+        force.Normalize();
+        GetComponent<Rigidbody2D>().AddForce(force * magnitude);
+    }
+}

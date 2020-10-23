3df7c3099875ad8eeb8b005872130e0a508de856
Joben Maratas <joben_maratas@yahoo.com.ph>
2020-10-23 23:26:39 +0800

    Added CarryController script, Cleantoy script, fixed the playermovement and fixed the clean up toys minigame

---
+++ Assets/Scripts/Game/CleanUpMinigame/CarryController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarryController : MonoBehaviour
+{
+    [Header("Variables")]
+    [SerializeField] CleanToy cleanToy;
+    public Transform itemDetector;
+    public Transform itemCarrier;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If the player collides with the item, the player will grab the item
+        if (collision.gameObject.CompareTag("Item"))
+        {
+            cleanToy = collision.gameObject.GetComponent<CleanToy>();
+            collision.gameObject.transform.parent = itemCarrier;
+            collision.gameObject.transform.position = itemCarrier.position;
+            collision.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+
+            // If the item is placed, the player will no longer hold the item.
+            if (cleanToy.isPlaced == true)
+            {
+                collision.gameObject.transform.parent = cleanToy.itemHolder;
+                collision.gameObject.transform.position = cleanToy.itemHolder.position;
+                collision.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            }
+
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        // If the item is placed and the player is far from the holder.
+        if (cleanToy.isPlaced == true)
+        {
+            collision.gameObject.transform.parent = cleanToy.itemHolder;
+            collision.gameObject.transform.position = cleanToy.itemHolder.position;
+            collision.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/CleanUpMinigame/CleanToy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanToy : MonoBehaviour
+{
+    [Header("Variables")]
+    [SerializeField] CarryController carryController;
+    [SerializeField] public Transform itemHolder;
+    public bool isPlaced;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the object is near the item holder, the object will automatically be placed.
+        if (Mathf.Abs(transform.position.x - itemHolder.transform.position.x) <= 1.2f &&
+            Mathf.Abs(transform.position.y - itemHolder.transform.position.y) <= 1.2f)
+        {
+            ItemHolderPosition();
+            isPlaced = true;
+        }
+        else
+        {
+            isPlaced = false;
+        }
+    }
+
+    public void ItemHolderPosition()
+    {
+        // To put the item in the holder.
+        transform.position = new Vector2(itemHolder.transform.position.x, itemHolder.transform.position.y);
+    }
+}

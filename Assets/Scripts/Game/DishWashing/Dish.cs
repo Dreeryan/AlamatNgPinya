22996af4c2bbb1d367afcf275a058d2e49a26a1d
Joben Maratas <joben_maratas@yahoo.com.ph>
2020-10-25 23:45:34 +0800

    Added washing the dishes minigame

---
+++ Assets/Scripts/Game/DishWashing/Dish.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Dish : MonoBehaviour
+{
+    [SerializeField] Sponge sponge;
+
+    [Header("Variables")]
+    [SerializeField] public float currentCleanRate;
+    [SerializeField] Transform dishRack;
+    [SerializeField] float maxRate;
+
+    [Header("UI")]
+    [SerializeField] TextMeshProUGUI cleanRateText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cleanRateText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the current clean rate is equal to the max clean rate
+        if (currentCleanRate > maxRate)
+        {
+            currentCleanRate = maxRate;
+            // The clean dish will be put into the rack
+            transform.position = new Vector2(dishRack.transform.position.x, dishRack.transform.position.y);
+        }
+
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        // If the dish is staying within the sponge
+        if (collision.gameObject.CompareTag("Sponge"))
+        {
+            cleanRateText.gameObject.SetActive(true);
+            sponge = collision.gameObject.GetComponent<Sponge>();
+            cleanRateText.text = "Current clean rate: " + currentCleanRate.ToString("f0") + "%";
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Dish"))
+        {
+            cleanRateText.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/DishWashing/Sponge.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Sponge : MonoBehaviour
+{
+    [SerializeField] Dish dish;
+
+    private Vector2 mousePos;
+    private Vector2 currentPosition;
+    public static bool isPlaced;
+
+    [Header("Variables")]
+    [SerializeField] float fillRate;
+
+    void Start()
+    {
+        currentPosition = transform.position;
+    }
+
+    void OnMouseDrag()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
+    }
+
+    void OnMouseUp()
+    {
+        // The sponge will be back to its current position
+        transform.position = new Vector2(currentPosition.x, currentPosition.y);
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        // If the sponge is staying around the dish
+        if (collision.gameObject.CompareTag("Dish"))
+        {
+            dish = collision.gameObject.GetComponent<Dish>();
+            dish.currentCleanRate += fillRate * Time.deltaTime;
+        }
+    }
+}

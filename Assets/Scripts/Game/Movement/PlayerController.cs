2824c64d36390db65c1926a5580d96d25c833c8b
Joben Maratas <joben_maratas@yahoo.com.ph>
2020-10-29 14:01:58 +0800

    Fixed the clean up toys minigame (where the player wasn't moving)

--- Assets/Scripts/Game/Movement/PlayerController.cs
+++ Assets/Scripts/Game/Movement/PlayerController.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Variables")]
-    [SerializeField] private float speed = 5;
+    [SerializeField] private float moveSpeed = 5f;
+    private Vector3 targetPoint;
 
-    [SerializeField] private Vector3 targetPoint;
     public bool isMoving;
 
-    // Update is called once per frame
     void Update()
     {
-        // Left click to move
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
+            // Player will go to the clicked area.
             targetPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            targetPoint.z = transform.position.z;
+            transform.position = Vector2.MoveTowards(transform.position, targetPoint, moveSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPoint);
+
+            Vector3 currentRotation = transform.localEulerAngles;
+            currentRotation.z = 0;
+            transform.localEulerAngles = currentRotation;
+
             isMoving = true;
         }
 
+        // If the mouse position is less than -1, the player's y rotation axis will be flipped to 180
+        if (targetPoint.x < -1)
+        {
+            transform.localEulerAngles = new Vector3(0, 180, 0);
+        }
+
+        // // If the mouse position is greater than -1, the player's y rotation axis will be flipped to 0
+        else
+        {
+            transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
+
+        // To move the player
         if (isMoving)
         {
             Movement();
@@ -29,13 +47,14 @@ public class PlayerController : MonoBehaviour
 
     void Movement()
     {
-        // Player will go to the desired position
-        transform.position = Vector3.MoveTowards(transform.position, targetPoint, speed * Time.deltaTime);
-        transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPoint);
-		
+        // To move the player to the desired position
+        transform.position = Vector2.MoveTowards(transform.position, targetPoint, moveSpeed * Time.deltaTime);
+
+        //A: Be careful with exact checks like this, floating point will cause bugs
         if (transform.position == targetPoint)
         {
             isMoving = false;
+
         }
     }
 }

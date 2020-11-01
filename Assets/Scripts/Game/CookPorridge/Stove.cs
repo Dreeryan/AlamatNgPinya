79e22f177ce977e187143e963ee1f330afd1daff
Joben Maratas <joben_maratas@yahoo.com.ph>
2020-11-01 14:52:53 +0800

    Added Stove temperature

---
+++ Assets/Scripts/Game/CookPorridge/Stove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Stove : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+    [SerializeField] private TextMeshProUGUI temperatureText;
+    [SerializeField] private float stoveTemperature;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GameObject.Find("Stove Slider").GetComponent<Slider>();
+        temperatureText = GameObject.Find("Temperature Text").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateUI();
+    }
+
+    public void UpdatePercent()
+    {
+        stoveTemperature = Mathf.RoundToInt(slider.value * 100);
+    }
+
+    void UpdateUI()
+    {
+        temperatureText.text = stoveTemperature.ToString("f0") + "°C";
+    }
+}

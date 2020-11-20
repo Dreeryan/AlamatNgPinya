b624de9d1c222b64578a299bb8ac37396812d65b
Raphael Antonio <raphaelsebastian.antonio@benilde.edu.ph>
2020-11-20 14:58:44 +0800

    Resized UI in Main and Minigame scenes. Fixed ShakeObject bug. Removed SweepingMinigame script.

--- Assets/Scripts/Game/ShakeObject.cs
+++ Assets/Scripts/Game/ShakeObject.cs
@@ -5,58 +5,50 @@ using UnityEngine.Events;
 
 public class ShakeObject : MonoBehaviour
 {
-    public UnityEvent   OnShake;
-    public Transform    shakeObject;
+    public UnityEvent OnShake;
+    public Transform shakeObject;
 
-    private Vector3     lastPosition;
-    private Vector3     currentPosition;
-    private float       threshold = 0.0f;
+    private bool isMouseDrag;
+    private Vector2 currentPosition;
+    private Vector2 mousePos;
+    private Vector2 previousPos = Vector2.zero;
+    private float threshold = 0.1f;
 
     // This is for testing
     [SerializeField] private bool isShaking;
 
-
-    private void Start()
-    {
-        currentPosition = transform.position;
-        lastPosition = shakeObject.position;
-    }
-
     void Update()
     {
-        currentPosition = shakeObject.position - lastPosition;
-
-        // For shaking left and right
-        if (currentPosition.x > threshold || currentPosition.x < -threshold)
-        {
-            // Updates last position
-            lastPosition = shakeObject.position;
-
+        if (isMouseDrag)
             isShaking = true;
-        }
 
-        // For shaking up and down
-        if (currentPosition.y > threshold || currentPosition.y < -threshold)
-        {
-            // Updates last position
-            lastPosition = shakeObject.position;
-
-            isShaking = true;   
-        }
-
-        // Called when shaking stops
-        if (currentPosition == lastPosition)
-        {
-            // Updates last position
-            lastPosition = shakeObject.position;
+        else
             isShaking = false;
-        }
 
         if (isShaking)
-        {
             OnShake.Invoke();
-        }
     }
+
+    void OnMouseDrag()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
+        isMouseDrag = true;
+    }
+
+    void OnMouseUp()
+    {
+        // The object will snap back to it's original position
+        transform.position = currentPosition;
+
+        isMouseDrag = false;
+    }
+
+    void OnMouseDown()
+    {
+        previousPos = Input.mousePosition;
+    }
+
 
     public void Test()
     {

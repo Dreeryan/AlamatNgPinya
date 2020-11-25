8c061dc2f47a7cc0e16c22ccb7406cfb90b10a22
Joben Maratas <joben_maratas@yahoo.com.ph>
2020-11-25 16:24:42 +0800

    Implemented Fire animation and slider to cook the porridge

--- Assets/Scripts/Game/CookPorridge/Porridge.cs
+++ Assets/Scripts/Game/CookPorridge/Porridge.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Porridge : MonoBehaviour
@@ -8,100 +9,61 @@ public class Porridge : MonoBehaviour
     public Animator animator;
     private float currentTemp = 0.0f;
     private float maxTemp = 100.0f;
-    private float minTemp = 0.0f;
+    private int tempValue;
 
     [Header("UI")]
-    [SerializeField] private TextMeshProUGUI tempText;
-    [SerializeField] private GameObject winningPanel;
-    [SerializeField] private float secondsToWin;
-
+    [SerializeField] private Slider porridgeSlider;
+    
     [Header("Porridge Variables")]
-    [SerializeField] private float coldTemp;
-    [SerializeField] private float rightTemp;
-    [SerializeField] private float hotTemp;
     [SerializeField] private float addTemperature;
     [SerializeField] private float decreaseTemperature;
-    [SerializeField] private GameObject fireObj;
+    [SerializeField] private float coldTemp;
+    [SerializeField] private float cookTemp;
+    [SerializeField] private float hotTemp;
 
-    private bool isCold = false;
-    private bool isRight = false;
-    private bool isHot = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (fireObj != null) fireObj.SetActive(false);
-        if (winningPanel != null) winningPanel.SetActive(false);
-    }
+    public bool cold = false;
+    public bool right = false;
+    public bool hot = false;
 
     void Update()
     {
         UpdateTemp();
-
-        if (isRight)
-        {
-            StartCoroutine("WinCountdown");
-        }
-        else
-        {
-            StopCoroutine("WinCountdown");
-        }
     }
 
     public void UpdateTemp()
     {
+        currentTemp = (int)(porridgeSlider.value * 100);
+
         if (Input.GetMouseButton(0))
         {
-            currentTemp += addTemperature * Time.deltaTime;
+            porridgeSlider.value += addTemperature * Time.deltaTime;
+            //currentTemp += addTemperature * Time.deltaTime;
 
-            if (currentTemp >= maxTemp) currentTemp = maxTemp;
+            if (currentTemp >= maxTemp)
+            {
+                currentTemp = maxTemp;
+            }
         }
 
         else
         {
-            currentTemp -= decreaseTemperature * Time.deltaTime;
+            porridgeSlider.value -= decreaseTemperature * Time.deltaTime;
 
-            if (currentTemp <= minTemp) currentTemp = minTemp;
+            if (currentTemp <= 0)
+            {
+                currentTemp = 0;
+            }
         }
+
+        animator.SetFloat("Temp", currentTemp);
 
         if (currentTemp > 0)
         {
-            fireObj.SetActive(true);
-            animator.SetBool("isCooking", true);
+            animator.SetBool("isFireOn", true);
         }
-
-        if (currentTemp < coldTemp)
+        else
         {
-            isCold = true;
-            isRight = false;
-            isHot = false;
+            animator.SetBool("isFireOn", false);
         }
-
-        else if (currentTemp < rightTemp)
-        {
-            isRight = true;
-            isCold = false;
-            isHot = false;
-        }
-
-        else if (currentTemp < hotTemp)
-        {
-            isHot = true;
-            isRight = false;
-            isCold = false;
-        }
-
-        UpdateUI();
-    }
-
-    void UpdateUI()
-    {
-        if (tempText != null) tempText.text = currentTemp.ToString("f0") + "°C";
-    }
-
-    IEnumerator WinCountdown()
-    {
-        yield return new WaitForSeconds(secondsToWin);
-        winningPanel.SetActive(true);
     }
 }

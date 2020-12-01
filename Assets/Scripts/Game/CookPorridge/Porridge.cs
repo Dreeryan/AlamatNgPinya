6fe6e1c831fc4e7f2bfea7bb5d8f09f596ce9efc
Joben Maratas <joben_maratas@yahoo.com.ph>
2020-12-01 17:10:28 +0800

    Implemented the win state for the cook the porridge and fixed the pot cover animation

--- Assets/Scripts/Game/CookPorridge/Porridge.cs
+++ Assets/Scripts/Game/CookPorridge/Porridge.cs
@@ -8,6 +8,8 @@ public class Porridge : MonoBehaviour
 {
     private float currentTemp = 0.0f;
     private float maxTemp = 100.0f;
+    private float rightTemp = 30f;
+    private bool isRightTemp = false;
 
     [Header("Animator")]
     public Animator fireAnimator;
@@ -15,21 +17,42 @@ public class Porridge : MonoBehaviour
 
     [Header("UI")]
     [SerializeField] private Slider porridgeSlider;
-    
+    [SerializeField] private GameObject winningPanel;
+
     [Header("Porridge Variables")]
     [SerializeField] private float addTemperature;
     [SerializeField] private float decreaseTemperature;
     [SerializeField] private float coldTemp;
     [SerializeField] private float cookTemp;
     [SerializeField] private float hotTemp;
+    [SerializeField] private float secondsToUndercooked;
+    [SerializeField] private float secondsToCooked;
+    [SerializeField] private float secondsToWin;
+
+    void Start()
+    {
+        if (winningPanel != null) winningPanel.SetActive(false);
+    }
 
     void Update()
     {
         UpdateTemp();
+        print(currentTemp);
+
+        // If its at the right temp
+        if (isRightTemp)
+        {
+            StartCoroutine("RightTempCountdown");
+        }
+        else
+        {
+            StopCoroutine("RightTempCountdown");
+        }
     }
 
     public void UpdateTemp()
     {
+        // Getting the current temp from the slider value
         currentTemp = (int)(porridgeSlider.value * 100);
 
         if (Input.GetMouseButton(0))
@@ -58,7 +81,6 @@ public class Porridge : MonoBehaviour
         if (currentTemp > 0)
         {
             fireAnimator.SetBool("isFireOn", true);
-            potCoverAnimator.SetBool("isFireOn", true);
         }
         else
         {
@@ -69,5 +91,24 @@ public class Porridge : MonoBehaviour
         {
             potCoverAnimator.SetBool("isFireOn", false);
         }
+
+        if (currentTemp > rightTemp)
+        {
+            isRightTemp = true;
+        }
+        else
+        {
+            isRightTemp = false;
+        }
+    }
+
+    IEnumerator RightTempCountdown()
+    {
+        yield return new WaitForSeconds(secondsToUndercooked);
+        potCoverAnimator.SetBool("isFireOn", true);
+        yield return new WaitForSeconds(secondsToCooked);
+        potCoverAnimator.SetBool("isCooked", true);
+        yield return new WaitForSeconds(secondsToWin);
+        winningPanel.SetActive(true);
     }
 }

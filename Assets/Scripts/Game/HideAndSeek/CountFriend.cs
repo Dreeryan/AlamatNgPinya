f54aa1a0b5b402a4033dbfd2de5a0b7d08e28145
Raphael Antonio <raphaelsebastian.antonio@benilde.edu.ph>
2020-12-03 09:35:42 +0800

    Created Hide and Seek prototype

---
+++ Assets/Scripts/Game/HideAndSeek/CountFriend.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountFriend : MonoBehaviour
+{
+    [SerializeField] private FriendFound friendFound;
+
+    public void CountFriends()
+    {
+        friendFound.friendsFound++;
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/Game/HideAndSeek/FriendFound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendFound : MonoBehaviour
+{
+    public int friendsFound;
+    private int previousFriendsFound;
+
+    [SerializeField] private int friendsGoal;
+    [SerializeField] private GameObject winScreen;
+
+    private void Start()
+    {
+        winScreen.SetActive(false);
+    }
+
+    void Update()
+    {
+        friendsGoal = GameObject.FindGameObjectsWithTag("Friend").Length;
+
+        if (friendsFound > previousFriendsFound)
+        {
+            previousFriendsFound = friendsFound;
+
+            if (friendsFound >= friendsGoal)
+            {
+                winScreen.SetActive(true);
+            }
+        }
+
+    }
+}

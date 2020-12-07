076be565672f6046dfec3409f90823dee7a6b4ed
Joben Maratas <joben_maratas@yahoo.com.ph>
2020-12-07 16:24:55 +0800

    Implemented player movement for Tag minigame

---
+++ Assets/Scripts/Game/Tag/PlayerTag.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTag : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private Vector3 targetPoint;
+    private bool isMoving = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            targetPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPoint.z = transform.position.z;
+            isMoving = true;
+        }
+
+        if (isMoving)
+        {
+            Movement();
+        }
+    }
+
+    void Movement()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPoint, moveSpeed * Time.deltaTime);
+
+        if (transform.position == targetPoint)
+        {
+            isMoving = false;
+        }
+    }
+}

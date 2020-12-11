d2b5417bb5021ff0a7a00763186cf4fd6ab09a47
Raphael Antonio <raphaelsebastian.antonio@benilde.edu.ph>
2020-12-11 10:24:31 +0800

    Added Progression Bar into FeedAnimals scene.

---
+++ Assets/Scripts/Game/FeedAnimals/ProgressionBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressionBar : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] GameObject WinScreen;
+
+    private int maxFood = 100;
+
+    private void Start()
+    {
+        WinScreen.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (slider.value >= maxFood)
+        {
+            WinScreen.SetActive(true);
+        }
+    }
+
+    public void SetFood(int food)
+    {
+        slider.value = food;
+    }
+
+    public void AddFood()
+    {
+        slider.value++;
+    }
+}

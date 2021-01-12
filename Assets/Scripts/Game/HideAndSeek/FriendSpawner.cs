a63c6713e8aa2ba05f4d0f7c8286699759ffa235
Raphael Antonio <raphaelsebastian.antonio@benilde.edu.ph>
2021-01-12 10:30:07 +0800

    Fixed no friends spawning bug in HideAndSeek minigame

--- Assets/Scripts/Game/HideAndSeek/FriendSpawner.cs
+++ Assets/Scripts/Game/HideAndSeek/FriendSpawner.cs
@@ -8,6 +8,7 @@ public class FriendSpawner : MonoBehaviour
 
     private int spawnCount;
 
+    [SerializeField] private PositionRandomizer positionRandomizer;
     [SerializeField] private List<GameObject>   friendPrefabs = new List<GameObject>();
     [SerializeField] private List<GameObject>   spawnedItems  = new List<GameObject>();
 
@@ -17,7 +18,7 @@ public class FriendSpawner : MonoBehaviour
         foreach (GameObject friend in friendPrefabs)
         {
             // Sets hiding spots as spawn points
-            Vector3 hidingSpot = GetSpawnPosition();
+            Vector3 hidingSpot = positionRandomizer.GetSpawnPosition();
 
             GameObject newFriends = Instantiate(friend, hidingSpot, Quaternion.identity);
 

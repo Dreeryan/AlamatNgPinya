0b41c8bebfcb74a623ca60698400d059b34ca2b2
Raphael Antonio <raphaelsebastian.antonio@benilde.edu.ph>
2021-01-15 11:37:51 +0800

    Implemented OccupiedChecker script in HideAndSeek scene (WIP)

--- Assets/Scripts/Game/OccupiedChecker.cs
+++ Assets/Scripts/Game/OccupiedChecker.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class OccupiedChecker : MonoBehaviour
 {
+    [SerializeField] private FriendSpawner friendSpawner;
+
+    public bool isOccupied; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isOccupied = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isOccupied)
+            friendSpawner.GetSpawnPosition();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Friend")
+        {
+            isOccupied = true;
+        }
     }
 }

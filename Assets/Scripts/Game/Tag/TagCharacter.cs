1098029614885512afd967466547f4118a98b0e8
LuMiGrA <luismiguelaraneta@gmail.com>
2021-01-19 10:43:16 +0800

    Moved win condition to TagManager class

---
+++ Assets/Scripts/Game/Tag/TagCharacter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TagCharacter : MonoBehaviour
+{
+    public class OnTagged : UnityEvent<TagCharacter> { }
+    public OnTagged onTagged = new OnTagged();
+
+    private bool    isTagged = false;
+    public bool     IsTagged
+    {
+        get { return isTagged; }
+        set { isTagged = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void HasBeenTagged()
+    {
+        onTagged.Invoke(this);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        TagCharacter collided = collision.collider.GetComponent<TagCharacter>();
+        if (collided != null && collided.IsTagged) HasBeenTagged();
+    }
+}
---
+++ Assets/Scripts/Game/Tag/TagManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagManager : MonoBehaviour
+{
+    [SerializeField] private GameObject     winScreen;
+
+    [SerializeField] private TagCharacter[] kids;
+    [SerializeField] private TagCharacter   startingTagged;
+    private TagCharacter currentTagged;
+
+    void Start()
+    {
+        // Adds listener for when a kid gets tagged
+        foreach (TagCharacter kid in kids)
+        {
+            kid.onTagged.AddListener(OnCurrentTaggedChange);
+        }
+
+        currentTagged = startingTagged;
+        currentTagged.IsTagged = true;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // Changes the current tagged kid
+    void OnCurrentTaggedChange(TagCharacter newTagged) 
+    {
+        currentTagged.IsTagged = false;
+        currentTagged = newTagged;
+        currentTagged.IsTagged = true;
+
+        // If the current tagged is one of the other kids, complete the minigame
+        if (currentTagged.CompareTag("Enemy")) DisplayWinScreen();
+    }
+
+    void DisplayWinScreen() 
+    {
+        winScreen.SetActive(true);
+    }
+}

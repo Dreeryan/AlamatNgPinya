00720a7c8bd576615486e2790b98f238c1787735
Arthur Kyle Estuar <arthur@altitude-games.com>
2021-02-02 11:57:28 +0800

    Add persistent scene; Add boot starter

---
+++ Assets/Scripts/BootStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BootStart : MonoBehaviour
+{
+    private void Start()
+    {
+        SceneLoader loader = FindObjectOfType<SceneLoader>();
+
+        if (loader == null) return;
+
+        loader.ChangeScene("MainMenu");
+    }
+
+}

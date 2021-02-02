3b5c5e930fba3ad7440b8b458d6aaaa8cd0a2bc6
LuMiGrA <luismiguelaraneta@gmail.com>
2021-02-02 10:55:38 +0800

    Made a debug script that makes an AudioManager object if not already present

---
+++ Assets/Scripts/Game/DebugLoadAudio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugLoadAudio : MonoBehaviour
+{
+    [SerializeField] private AudioManager managerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (AudioManager.Instance == null) Instantiate(managerPrefab);
+    }
+}

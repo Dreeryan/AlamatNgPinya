dc6bada0c499c04e640d445719cef9539b5432ae
Arthur Kyle Estuar <arthur@altitude-games.com>
2021-02-02 12:06:08 +0800

    Add play game button
    Alternative play game button that will auto load from persistent

---
+++ Assets/Editor/MenuItems.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class MenuItems
+{
+    [MenuItem("Game/PLAY GAME")]
+    public static void PlayGame()
+    {
+        if (EditorApplication.isPlaying) return;
+
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene("Assets/Scenes/_Persistent.unity");
+
+        EditorApplication.isPlaying = true;
+    }
+}
+#endif

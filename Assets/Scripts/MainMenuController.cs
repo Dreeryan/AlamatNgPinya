f1459d04e721eb7a53ec898bc923ee8320e76330
Arthur Kyle Estuar <arthur@altitude-games.com>
2021-02-03 17:36:48 +0800

    Add main menu controller

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onStart;
+
+    private void Start()
+    {
+        onStart?.Invoke();
+    }
+
+
+}

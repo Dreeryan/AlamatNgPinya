f93425f90d768c0f6c2171334ff3bc0783d8cec9
Arthur Kyle Estuar <arthur@altitude-games.com>
2021-02-04 14:32:36 +0800

    Add motivation manager and UI handler

---
+++ Assets/Scripts/Manager/MotivationManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotivationManager : BaseManager<MotivationManager>
+{
+    [SerializeField] private int maxMotivation;
+
+    private int currMotivation;
+
+    public int MaxMotivation => maxMotivation;
+    public int CurrMotivation => currMotivation;
+
+    public static System.Action<int> OnMotivationUpdated;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        currMotivation = maxMotivation;
+    }
+
+    public void IncrementMotivation(int delta)
+    {
+        currMotivation += delta;
+
+        currMotivation = Mathf.Clamp(currMotivation
+            , 0
+            , maxMotivation);
+
+        OnMotivationUpdated?.Invoke(currMotivation);
+    }
+
+    public void ResetMotivation()
+    {
+        currMotivation = maxMotivation;
+    }
+}
---
+++ Assets/Scripts/UI/Main/MotivationMeter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MotivationMeter : MonoBehaviour
+{
+    [SerializeField] private Image fillImage;
+
+    private void OnEnable()
+    {
+        MotivationManager.OnMotivationUpdated += OnMotivationUpdated;
+    }
+
+    private void OnDisable()
+    {
+        if (MotivationManager.IsShuttingDown) return;
+
+        MotivationManager.OnMotivationUpdated -= OnMotivationUpdated;
+    }
+
+    private void OnMotivationUpdated(int newVal)
+    {
+        if (fillImage == null) return;
+
+        fillImage.fillAmount = newVal;
+    }
+}

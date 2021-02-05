ba90514d6d57f7de2873898a996c016a4301e828
Arthur Kyle Estuar <arthur@altitude-games.com>
2021-02-05 16:46:02 +0800

    Add score manager

---
+++ Assets/Scripts/Database/ScoreData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ScoreData : BaseData
+{
+    [SerializeField] private string minigameId;
+    [SerializeField] private float  baseScoreValue;
+    [SerializeField] private float  secondsTarget;
+
+    public string MinigameId => minigameId;
+    public float BaseScoreValue => baseScoreValue;
+    public float SecondsTarget => secondsTarget;
+
+    public float GetResultingScore(float secondsTaken)
+    {
+        return baseScoreValue * (secondsTarget / secondsTaken);
+    }
+}
---
+++ Assets/Scripts/Manager/ScoreManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : BaseManager<ScoreManager>
+{
+    [SerializeField] private ScoreDatabase scoreDB;
+
+    public float GetFinalScore(string minigameId, float secsTaken)
+    {
+        if (scoreDB == null || scoreDB.GetData(minigameId) == null)
+            return -1;
+
+        return scoreDB.GetData(minigameId).GetResultingScore(secsTaken);
+    }
+}

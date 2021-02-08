f9352a74756240f8eb8d27f2a71d11cfc8058514
Arthur Kyle Estuar <arthur@altitude-games.com>
2021-02-08 15:29:16 +0800

    Remove minigame id

--- Assets/Scripts/Database/ScoreData.cs
+++ Assets/Scripts/Database/ScoreData.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 [System.Serializable]
 public class ScoreData : BaseData
 {
-    [SerializeField] private string minigameId;
     [SerializeField] private float  baseScoreValue;
     [SerializeField] private float  secondsTarget;
-
-    public string MinigameId => minigameId;
     public float BaseScoreValue => baseScoreValue;
     public float SecondsTarget => secondsTarget;
 

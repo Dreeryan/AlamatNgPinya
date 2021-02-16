a7b9b4e70d604c4a1e5f73b74725f7ddd6175eca
Clifton Kyle Marasigan <cliftonkyle.marasigan@beniled.edu.ph>
2021-02-16 18:01:06 +0800

    -Working on showing objectives on ui (WIP)

--- Assets/Scripts/Manager/TaskListManager.cs
+++ Assets/Scripts/Manager/TaskListManager.cs
@@ -1,22 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 
 
 public class TaskListManager : BaseManager<TaskListManager>
 {
     [SerializeField] private TaskDatabase taskDB;
 
-    private List<string> taskList = new List<string>();
+    public List<string> taskList { get; private set; } = new List<string>();
 
+    public UnityEvent    onTaskRemoved;
+
+    private void Awake()
+    {
+        //Get all tasks and put it in the list of this manager  
+        GetTasksFromDataBase();
+    }
     protected override void Start()
     {
         base.Start();
-        GetTasksFromDataBase();
-        //Get all tasks and put it in the list of this manager  
+
+
 
     }
+
+
     public string GetTask(string TaskId)
     {
         return taskDB.GetData(TaskId).GetTask();
@@ -28,7 +37,11 @@ public class TaskListManager : BaseManager<TaskListManager>
 
         for (int i = 0; i < taskList.Count; i++)
         {
-            if (task == taskList[i]) taskList.Remove(taskList[i]);
+            if (task == taskList[i])
+            {
+                this.onTaskRemoved.Invoke();
+                taskList.Remove(taskList[i]);
+            }
         }
     }
 

8edbee57662c58042f9e839aee3de1ec4120e99f
Clifton Kyle Marasigan <cliftonkyle.marasigan@beniled.edu.ph>
2021-02-16 12:18:51 +0800

    Added task database Added task data Added Tasklist Manager Script and Prefab (WIP)

---
+++ Assets/Scripts/Manager/TaskListManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class TaskListManager : BaseManager<TaskListManager>
+{
+    [SerializeField] private TaskDatabase taskDB;
+
+    public string GetTask(string TaskId)
+    {
+        return taskDB.GetData(TaskId).GetTask();
+    }
+
+    public void RemoveTaskFromList(string TaskId)
+    {
+
+    }
+
+}

aa80a5a7cd886b71b6fd2e8a198ed8e22e3478f1
Clifton Kyle Marasigan <cliftonkyle.marasigan@beniled.edu.ph>
2021-02-17 18:20:27 +0800

    - Implemented pineapple transforming mechanic that acts as a life count feature - Added tentative pineapple transformer sprites at the upper right of the UI

--- Assets/PineappleTransformer.cs
+++ Assets/PineappleTransformer.cs
@@ -2,55 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using System.Linq;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class PineappleTransformer : MonoBehaviour
 {
     public UnityEvent onPineappleTransform = new UnityEvent();
+    public UnityEvent onCompletePineappleTransformation = new UnityEvent();
 
-    [SerializeField] private int MaxNumberToAsk;
+    [SerializeField] private Sprite[] pineapplePlayerSprites;
 
-    private int    currentAskedAmount=0;
-    public string  spriteSheetName;
-    private string loadedSpriteSheetName;
-
-    private Dictionary<string, Sprite> spriteSheet;
-    private SpriteRenderer spriteRenderer;
+    [SerializeField] private int maxNumberToAsk;
+    private int                  currentAskedAmount=0;
+    private Image                imageComponent;
 
     // Use this for initialization
     private void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    private void LateUpdate()
-    {
-        spriteRenderer.sprite = spriteSheet[this.spriteRenderer.sprite.name];
-    }
-
-    private void ChangeSpriteSheet()
-    {
-        //Change resources to something else later
-        var sprites = Resources.LoadAll<Sprite>(this.spriteSheetName);
-       // var sprites = (Sprite[])AssetDatabase.LoadAllAssetsAtPath("Assets/Sprites/Player/Character_Pina_PinaRunning.png");
-        this.spriteSheet = sprites.ToDictionary(x => x.name, x => x);
-        this.loadedSpriteSheetName = this.spriteSheetName;
+        imageComponent = GetComponent<Image>();
     }
 
     public void TurnToPineapple()
     {
-        Debug.Log("GAME OVER");
-        if (currentAskedAmount < MaxNumberToAsk)
-        {
-            currentAskedAmount += 1;
-            return;
-        }
-        else
-        {
-            this.ChangeSpriteSheet();
-            onPineappleTransform.Invoke();
-        }
+        if (currentAskedAmount >= maxNumberToAsk) return;
+
+        currentAskedAmount += 1;
+        this.imageComponent.sprite = pineapplePlayerSprites[currentAskedAmount];
+        onPineappleTransform.Invoke();
+
+        if (currentAskedAmount == maxNumberToAsk) onCompletePineappleTransformation.Invoke();
+
     }
 }
 

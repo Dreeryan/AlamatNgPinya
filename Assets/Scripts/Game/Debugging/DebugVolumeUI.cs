751ec7d44c2c20e597c561c9ec770cb3d0fc0818
LuMiGrA <luismiguelaraneta@gmail.com>
2021-02-17 09:47:00 +0800

    Improved debug volume controller (now supports all 3 mix groups)

---
+++ Assets/Scripts/Game/Debugging/DebugVolumeUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugVolumeUI : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    [SerializeField] private KeyCode toggleKey = KeyCode.F2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(toggleKey)) panel.SetActive(!panel.activeSelf);
+    }
+}

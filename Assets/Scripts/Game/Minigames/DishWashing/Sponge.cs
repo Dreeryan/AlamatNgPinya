2cd16203fe90a7502c63c6faad88c40ac29235be
LuMiGrA <luismiguelaraneta@gmail.com>
2021-02-26 14:55:40 +0800

    Fixed audio triggers for dishwashing

--- Assets/Scripts/Game/Minigames/DishWashing/Sponge.cs
+++ Assets/Scripts/Game/Minigames/DishWashing/Sponge.cs
@@ -6,37 +6,50 @@ using TMPro;
 
 public class Sponge : MonoBehaviour
 {
-    [SerializeField] private UnityEvent OnScrubbingDish;
+    [SerializeField] private Draggable   draggable;
 
-    private Dish        dish;
-    private Draggable   draggable;
+    private Dish dish;
+    private bool isPlayingAudio;
 
     [Header("Sponge Settings")]
     [SerializeField] private float      drainRate;
-    [SerializeField] private float      dragTreshold = 0.01f;
 
     [Header("UI")]
     [SerializeField] TextMeshProUGUI    guideText;
 
+    [SerializeField] private UnityEvent onScrubbingDish;
+    [SerializeField] private UnityEvent onStopScrubbing;
+    
     void Start()
     {
-        draggable = GetComponent<Draggable>();
-
+        if (draggable == null) draggable = GetComponent<Draggable>();
         if (guideText != null) guideText.gameObject.SetActive(true);
     }
 
     void Update()
     {
-        if (Vector2.Distance(draggable.PrevMousePos,
-            draggable.CurMousePos) >= dragTreshold && dish != null)
+        if (draggable.HasMovedEnough && dish != null)
         {
-            OnScrubbingDish?.Invoke();
+            if (!isPlayingAudio)
+            {
+                onScrubbingDish?.Invoke();
+                isPlayingAudio = true;
+            }
             dish.ReduceDirtRate(drainRate * Time.deltaTime);
+        }
+        else
+        {
+            if (isPlayingAudio)
+            {
+                onStopScrubbing?.Invoke();
+                isPlayingAudio = false;
+            }
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
+        //if (dish != null) return;
         // Checks if the collided object is a dirty dish
         if (collision.gameObject.GetComponent<Dish>() == null) return;
 

af1d373b8c1425e2fe32274817527aa8ad78c08a
Clifton Kyle Marasigan <cliftonkyle.marasigan@beniled.edu.ph>
2021-03-04 11:15:01 +0800

    - Adding gameover scene with pineapple transformer (WIP)

--- Assets/PineappleTransformer.cs
+++ Assets/PineappleTransformer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
 using UnityEngine.Events;
-
+using UnityEngine.SceneManagement;
 public class PineappleTransformer : MonoBehaviour
 {
     public UnityEvent onPineappleTransform = new UnityEvent();
@@ -30,8 +30,12 @@ public class PineappleTransformer : MonoBehaviour
         this.imageComponent.sprite = pineapplePlayerSprites[currentAskedAmount];
         onPineappleTransform.Invoke();
 
-        if (currentAskedAmount == maxNumberToAsk) onCompletePineappleTransformation.Invoke();
+        if (currentAskedAmount == maxNumberToAsk)
+        {
 
+            onCompletePineappleTransformation.Invoke();
+            SceneManager.LoadScene("GameOver", LoadSceneMode.Additive);
+        }
     }
 }
 

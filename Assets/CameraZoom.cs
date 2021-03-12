93fef54e46dd724c9f6f8e10ca0279f91f2bc9ca
Clifton Kyle Marasigan <cliftonkyle.marasigan@beniled.edu.ph>
2021-03-12 14:33:23 +0800

    - Added temporary camera fix for aspect ratios (WIP) - Fixed main scene to adjust to all aspect ratios

---
+++ Assets/CameraZoom.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public SpriteRenderer boundSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Zoom();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Zoom()
+    {
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float targetRatio = boundSprite.bounds.size.x / boundSprite.bounds.size.y;
+
+        if(screenRatio>= targetRatio)
+        {
+         //   Camera.main.orthographicSize = boundSprite.bounds.size.y / 2;
+            Camera.main.orthographicSize = boundSprite.bounds.size.y / 2;
+        }
+        else
+        {
+            float sizeDifference = targetRatio / screenRatio;
+            Camera.main.orthographicSize = boundSprite.bounds.size.y / 2 * sizeDifference;
+        }
+        //float orthoSize = boundSprite.bounds.size.x * Screen.height / Screen.width * 0.5f;
+
+        //Camera.main.orthographicSize = orthoSize;
+    }
+}

eacad442f2d0fe2b7306cd17021a6acf997234e5
Clifton <cliftonkyle.marasigan@benilde.edu.ph>
2021-03-19 10:49:16 +0800

    - Made motivation tweener script - Added motivation tweener at the end of every chore (WIP)

--- Assets/MotivationTweener.cs
+++ Assets/MotivationTweener.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using DG.Tweening;
 public class MotivationTweener : MonoBehaviour
 {
+    [SerializeField] private Image fillImage;
+    public float reductionValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fillImage.fillAmount = MotivationManager.Instance.FillRatio + 0.2f;
+        StartCoroutine(TweenToAmount());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator TweenToAmount()
     {
-        
+        yield return new WaitForSeconds(1f);
+        fillImage.DOFillAmount(MotivationManager.Instance.FillRatio, 1);
     }
 }
